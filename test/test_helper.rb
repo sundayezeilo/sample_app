8748b35c8bacbb9b23ba9a5f6b8004dcc88037c4
ezeilo-su <ezeilo.su@gmail.com>
2020-07-26 01:42:02 +0100

    Finish layout links and routing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,10 @@ require_relative '../config/environment'
 require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
+  fixtures :all
+  include ApplicationHelper
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
