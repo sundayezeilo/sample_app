ade7c6ca0ba9c12641026717ab21c4589fabb6cd
ezeilo-su <ezeilo.su@gmail.com>
2020-08-03 19:38:55 +0100

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+    # debugger
   end
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+      flash[:danger] = "Signup unsuccessful! Try again."
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  private :user_params
 end
