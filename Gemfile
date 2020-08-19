421b9c0c10a1d82b1d30892dcd91f0a5303af509
ezeilo-su <ezeilo.su@gmail.com>
2020-08-19 23:37:03 +0100

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'rails',      '6.0.3.2'
 gem 'bcrypt', '~> 3.1', '>= 3.1.15'
+gem 'faker', '~> 2.13'
+gem 'will_paginate', '~> 3.3'
+gem 'bootstrap-will_paginate', '~> 1.0'
 gem 'bootstrap-sass', '3.4.1'
 gem 'puma',       '4.3.5'
 gem 'sass-rails', '6.0.0'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
-      # Log the user in and redirect to the user's show page.
-      else
-      # Create an error message.
-      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
