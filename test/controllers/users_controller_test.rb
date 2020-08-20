7634b6f502fb93777cbe53a0186ec8d6cfa359db
ezeilo-su <ezeilo.su@gmail.com>
2020-08-20 22:08:38 +0100

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -24,8 +24,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect update when not logged in" do
-    patch user_path(@user), params: { user: { name: @user.name,
-    email: @user.email } }
+    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
   end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,11 +1,10 @@
 require 'test_helper'
-
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:michael)
     @non_admin = users(:archer)
   end
-  
+
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
     get users_path
@@ -18,7 +17,6 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
         assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
     end
-    
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
     end
