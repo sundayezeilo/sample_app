6cefaaeb4b87a9f6c02e9d435c6baee5da4d7755
ezeilo-su <ezeilo.su@gmail.com>
2020-08-30 23:25:05 +0100

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Create a main sample user.
-User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar", admin: true)
-  # Generate a bunch of additional users.
+User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar", admin: true,
+  activated: true,  activated_at: Time.zone.now)
+
+# Generate a bunch of additional users.
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name: name, email: email, password: password, password_confirmation: password)
+  User.create!(name: name, email: email, password: password, password_confirmation: password,
+    activated: true,  activated_at: Time.zone.now)
 end
