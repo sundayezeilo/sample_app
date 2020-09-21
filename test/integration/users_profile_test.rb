dbb54659b9cec8e1771426947ead568804eb676e
ezeilo-su <ezeilo.su@gmail.com>
2020-09-21 23:48:38 +0100

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -17,8 +17,16 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'div.pagination'
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
-    end
-    
+    end    
     assert_select 'div.pagination', count:1
+  end
+
+  test "test profile stats on home page" do
+    log_in_as(@user)
+    get root_path(@user)
+    assert_template 'static_pages/home'
+    assert_select 'div.stats', count: 1
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
   end
 end
